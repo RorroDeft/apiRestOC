d84e09b35cbf8250fbcf5d87593e85538fe977eb
RodrigoFernandez <rfernandezm@ripley.com>
2020-11-27 12:01:36 -0300

    update en get

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,17 +2,24 @@ const Order = require("../models/Order");
 
 exports.getOrders = async (req, res) => {
     try {
-        const { date } = req.query;
+        const { date,enviroment} = req.query;
         console.log(date)
         const formatDate = date.split("-").join(",")
 
-        const orders = await Order.find({
+        const order = await Order.find({
+         
             dateRegistry: {
                 $gte: new Date(new Date(formatDate).setHours(00, 00, 00)),
                 $lt: new Date(new Date(formatDate).setHours(23, 59, 59))
-            }
-        }).sort({ dateRegistry: 'asc' })
-        res.json({ orders });
+
+            } ,
+            enviroment:(enviroment.toUpperCase())
+        })
+        .where("avaible").equals(true)
+        .sort({ dateRegistry: 'desc' })
+       // .select("ocnumber enviroment")
+       .select("ocnumber")
+        res.json({ order });
     } catch (error) {
         console.log(error);
         res.status(500).send("hubo un error")
@@ -28,7 +35,7 @@ exports.createOrder = async (req, res) => {
     }
 
     if (!enviroment.toUpperCase().match(/^(PRD|STAGING|PREPROD)$/)) {
-       return res.status(500).send ('invalid enviroment, must be prd,staging or preprod');
+        return res.status(500).send('invalid enviroment, must be prd,staging or preprod');
     }
     try {
         const newOrder = new Order(req.body);
@@ -41,16 +48,16 @@ exports.createOrder = async (req, res) => {
 }
 
 
-exports.changeStateOrder= async (req, res) => {
+exports.changeStateOrder = async (req, res) => {
     try {
         const { ocnumber } = req.body
         let order = await Order.findOne({ ocnumber }).where("avaible").equals(true)
-    
+
         if (!order) {
             throw new Error('Sorry, order not found');
         }
         req.body.avaible = false
-        order = await Order.findOneAndUpdate({ ocnumber : ocnumber }, req.body, { new: true});
+        order = await Order.findOneAndUpdate({ ocnumber: ocnumber }, req.body, { new: true });
         res.json({ order })
     } catch (error) {
         console.log(error);
